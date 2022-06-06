e37cdd32f6cc708d47865d849349fa40197f2d7b
John Paul Otim <jhnplotim@gmail.com>
2022-06-06 09:26:19 +0200

    feat: added question and answer model and admin
    - added new models
    - added and modified admin pages

--- lifeingermany/quickstart/admin.py
+++ lifeingermany/quickstart/admin.py
@@ -1,6 +1,58 @@
 from django.contrib import admin
-from quickstart.models import State
+from quickstart.models import State, Question, Answer
+from django.utils.html import format_html
 
 # Register your models here.
 
-admin.site.register(State)
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        if obj.image and hasattr(obj.image, 'url'):
+            html_string = f'<img src="{obj.image.url}" width="30" height="30" />'
+            return format_html(html_string)
+        else:
+            image = '-'
+            return image
+
+    def answer_count(self, obj):
+        return obj.answer_set.count()
+
+    def correct_count(self, obj):
+        return obj.answer_set.filter(is_correct=True).count()
+        
+
+    image_tag.short_description = 'Image'
+
+    answer_count.short_description = 'Answers'
+
+    correct_count.short_description = 'Correct Answers'
+
+    list_display = ['text','image_tag', 'state', 'answer_count', 'correct_count']
+
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    
+    def name_code(self, obj):
+        return f'{obj.name} ({obj.code})'
+
+    def image_tag(self, obj):
+        html_string = f'<img src="{obj.icon.url}" width="30" height="30" />'
+        return format_html(html_string)
+
+    def question_count(self, obj):
+        return obj.question_set.count()
+
+    image_tag.short_description = 'Icon'
+
+    name_code.short_description = 'Name (Code)'
+
+    question_count.short_description = 'Questions'
+
+    list_display = ['name_code','image_tag', 'question_count', 'added']
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['text', 'is_correct', 'question']
--- lifeingermany/quickstart/models.py
+++ lifeingermany/quickstart/models.py
@@ -7,7 +7,39 @@ alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters a
 
 # Create your models here.
 class State(models.Model):
-    id = models.TextField(max_length=2, primary_key=True, validators=[alphanumeric])
+    id = models.AutoField(primary_key=True)
     name = models.TextField(max_length=30, blank=False, null=False)
+    code = models.TextField(max_length=2, null=False, unique=True, validators=[alphanumeric])
     icon = models.ImageField(blank=False, null=False, upload_to='state_icons')
     added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField(blank=False,  null=False)
+    image = models.ImageField(blank=True, null=True, default=None, upload_to='question_images')
+    state = models.ForeignKey(State, on_delete=models.CASCADE, blank=True, null=True, default=None)
+
+    def __str__(self):
+      return f'{self.text}'
+
+    def set_image(self, image):
+        self.image = image
+
+    def set_state(self, state):
+        self.state = state
+
+class Answer(models.Model): 
+    id = models.AutoField
+    text = models.TextField(blank=False, null=False)
+    is_correct = models.BooleanField(default=False, null=False, blank=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=False, blank=False)
+    
+    def __str__(self):
+        return f'{self.text}'
+
+    def set_is_correct(self, is_correct):
+        self.is_correct = is_correct
