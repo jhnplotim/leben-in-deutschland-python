0f43bd73f5c0e792ec624ff42f1cad6f960b397c
John Paul Otim <jhnplotim@gmail.com>
2022-06-07 18:12:46 +0200

    refactor: reformated file
    - reformated file

--- lifeingermany/quickstart/models.py
+++ lifeingermany/quickstart/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.utils import timezone
 
-
 alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+
 
 # Create your models here.
 class State(models.Model):
@@ -19,12 +19,12 @@ class State(models.Model):
 
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
-    text = models.TextField(blank=False,  null=False)
+    text = models.TextField(blank=False, null=False)
     image = models.ImageField(blank=True, null=True, default=None, upload_to='question_images')
     state = models.ForeignKey(State, on_delete=models.CASCADE, blank=True, null=True, default=None)
 
     def __str__(self):
-      return f'{self.text}'
+        return f'{self.text}'
 
     def set_image(self, image):
         self.image = image
@@ -32,12 +32,13 @@ class Question(models.Model):
     def set_state(self, state):
         self.state = state
 
-class Answer(models.Model): 
+
+class Answer(models.Model):
     id = models.AutoField
     text = models.TextField(blank=False, null=False)
     is_correct = models.BooleanField(default=False, null=False, blank=False)
     question = models.ForeignKey(Question, on_delete=models.CASCADE, null=False, blank=False)
-    
+
     def __str__(self):
         return f'{self.text}'
 
