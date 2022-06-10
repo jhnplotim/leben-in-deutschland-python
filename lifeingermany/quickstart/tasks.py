81e83bcc631c38f2afdc6076dd113035a9be90a9
John Paul Otim <jhnplotim@gmail.com>
2022-06-10 09:58:09 +0200

    feat: initial celery configuration
    - initial celery configuration

---
+++ lifeingermany/quickstart/tasks.py
@@ -0,0 +1,29 @@
+from models import State, StateIcon
+
+from celery import shared_task
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
+
+
+@shared_task
+def count_states():
+    return State.objects.count()
+
+
+@shared_task
+def rename_state_with_code(state_code, name):
+    s = State.objects.get(code=state_code)
+    s.name = name
+    s.save()
