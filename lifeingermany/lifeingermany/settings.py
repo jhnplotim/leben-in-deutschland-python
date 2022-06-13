75d9764b66ce8cdb51283dd25ef93146ded521fd
John Paul Otim <jhnplotim@gmail.com>
2022-06-13 21:54:27 +0200

    feat: AWS backend storage added
    - AWS backend storage added

--- lifeingermany/lifeingermany/settings.py
+++ lifeingermany/lifeingermany/settings.py
@@ -125,11 +125,6 @@ USE_I18N = config('USE_I18N', default=True, cast=bool)
 USE_TZ = config('USE_TZ', default=True, cast=bool)
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -140,8 +135,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': config('REST_FRAMEWORK_PAGE_SIZE', default=10, cast=int)
 }
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # django setting.
 CACHES = {
@@ -169,3 +162,38 @@ CELERY_BROKER_URL = config('AMQP_URL', default=OTHER_AMQP_URL)
 import dj_database_url 
 db_from_env = dj_database_url.config(conn_max_age=500) 
 DATABASES['default'].update(db_from_env)
+
+
+if config('USE_S3', default=True, cast=bool):
+    # aws settings
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_ADDRESSING_STYLE = "virtual"
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_S3_REGION_NAME='eu-central-1'
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    # s3 static settings
+    AWS_STATIC_LOCATION = 'static'
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+    STATICFILES_STORAGE = 'lifeingermany.storage_backends.StaticStorage'
+    # s3 public media settings
+    AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
+    MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_PUBLIC_MEDIA_LOCATION)
+    DEFAULT_FILE_STORAGE = 'lifeingermany.storage_backends.PublicMediaStorage'
+    # s3 private media settings
+    AWS_PRIVATE_MEDIA_LOCATION = 'media/private'
+    PRIVATE_FILE_STORAGE = 'lifeingermany.storage_backends.PrivateMediaStorage'
+    
+else:
+    MEDIA_ROOT = BASE_DIR / 'media'
+    MEDIA_URL = "/media/"
+    
+    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_URL = "/static/"
+    
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'lifeingermany/static'),
+]
